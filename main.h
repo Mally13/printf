0e63b850ef47a72bde6f6df1005271bac36beb6e
HinnMachache <bennyhinn253.bh@gmail.com>
2023-06-27 10:25:48 -0700

    Address printing

--- main.h
+++ main.h
@@ -3,7 +3,6 @@
 
 #include <stdio.h>
 #include <stdarg.h>
-#include <unistd.h>
 /* A header file containing typedefs and function prototypes */
 
 
@@ -16,15 +15,10 @@
  */
 typedef void (*format_handler)(va_list printf_args, int *strLength);
 
-
 /* function prototypes */
 int _putchar(char c);
 void handle_int(va_list printf_args, int *strLength);
 void handle_base(va_list printf_args, int *strLength);
-void handle_unsignedDecimal(va_list printf_args, int *strLength);
-void handle_octal(va_list printf_args, int *strLength);
-void handle_lowerCaseHex(va_list printf_args, int *strLength);
-void handle_upperCaseHex(va_list printf_args, int *strLength);
 int _printf(const char *format, ...);
 void handle_string(va_list printf_args, int *strLength);
 void handle_char(va_list printf_args, int *strLength);
@@ -33,11 +27,7 @@ int binaryNumberConv(unsigned int decimalNumber);
 void print_octal(int number, int *strLength);
 void print_hexadecimal(int number, int *strLength);
 void print_decimal(int number, int *strLength);
-<<<<<<< HEAD
-void handle_specifier(const char *format, va_list printf_args, int *strLength);
-=======
 char *addressConv(unsigned long int num);
 void handle_address(va_list printf_args, int *strLength);
 
->>>>>>> hinn
 #endif
