f4d02432e1d64710b5254e4ef946de1662d173ee
Mally13 <mallymutuku13@gmail.com>
2023-06-30 23:21:56 +0300

    Soln task 5

--- task_5.c
+++ task_5.c
@@ -1,5 +1,17 @@
 #include "main.h"
 /**
+ * toHex - converts a sting to hex value
+ * @character: char to be converted
+ * Return: Hex value of capital char
+ */
+int toHex(char character)
+{
+	int hexValue;
+
+	hexValue = character - 'A' + 10;
+	return (hexValue);
+}
+/**
  * handle_S - Prints string and handles non-printable
  * characters
  * @printf_args: va_list arguments for printf
@@ -7,46 +19,43 @@
  */
 void handle_S(va_list printf_args, int *strLength)
 {
-	const char *string;
-	unsigned int hexVal, digit, divisor, i;
-	char cap;
+	char *string;
+	int i, j, newHex, digit, divisor, numLength = 0, holder;
 
 
 	string = va_arg(printf_args, char *);
-	while (*string)
+	if (!string)
+		return;
+	for (i = 0; string[i]; i++)
 	{
-		
-		if (*string == 92)
+		if (((string[i]) > 0 && (string[i]) < 32) || (string[i]) >= 127)
 		{
 			_putchar('\\');
-			string++;
-			(*strLength)++;
 			_putchar('x');
-			_putchar('\\');
-			(*strLength)++;
-
-			hexVal = *string;
-
+			(*strLength) += 2;
+			newHex = toHex(string[i]);
+			holder = newHex;
+			while (holder > 0)
+			{
+				holder /= 16;
+				numLength++;
+			}
 			divisor = 1;
-			for (i = 1; i < 2; i++)
+			for (j = 0; j < numLength; j++)
 				divisor *= 16;
-
 			while (divisor > 0)
 			{
-				digit = hexVal / divisor;
-				cap = 'A' + digit;
-				_putchar(cap);
-				_putchar('\\');
+				digit = newHex / divisor;
+				_putchar('0' + digit);
 				(*strLength)++;
-				hexVal %= divisor;
+				newHex %= divisor;
 				divisor /= 16;
 			}
 		}
 		else
 		{
-			_putchar(*string);
-			strLength++;
-			string++;
+			_putchar(string[i]);
+			(*strLength)++;
 		}
 	}
 }
